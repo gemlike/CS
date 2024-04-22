00c9ef6a4a8d701b659da8974813d596e8e36745
nguyenngocthe1001@gmail.com <nguyenngocthe1001@gmail.com>
2024-04-23 01:25:29 +0700

    Commit ngay 23 - 4 - 2024

---
+++ Test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+
+namespace NgocThe
+
+{
+
+    public class Product {
+        protected double price = 0;
+
+        // Phương thức ảo ProductInfo
+        public virtual void ProductInfo() {
+            Console.WriteLine($"Giá sản phẩm {price}");
+        }
+
+        public void TestProduct()
+        {
+            this.ProductInfo();
+        }
+    }
+
+    public class Milk:Product
+    {
+        public override void ProductInfo() {
+            Console.WriteLine($"Giá sản sữa {price}");
+        }
+    }
+
+    public class Test
+    {
+        
+        
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Milk mk = new Milk();
+            Product pt = new Product();
+            pt.ProductInfo();
+            mk.ProductInfo();
+            Console.ReadKey();
+        }
+    }
+
+}
