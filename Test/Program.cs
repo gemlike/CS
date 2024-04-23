b2eb9980dd56029c0f1ccb600e2a599a313b950d
nguyenngocthe1001@gmail.com <nguyenngocthe1001@gmail.com>
2024-04-23 22:36:17 +0700

    23/4/2024 Abstract class and Try catch

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Data.Common;
+using System.Net;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices.Marshalling;
 using System.Runtime.Intrinsics.Arm;
 using System.Text;
 
@@ -18,6 +22,7 @@ namespace NgocThe
         {
             this.ProductInfo();
         }
+
     }
 
     public class Milk:Product
@@ -27,17 +32,82 @@ namespace NgocThe
         }
     }
 
+    public abstract class Product1
+    {
+        public Product1()
+        {
+
+        }
+
+        public abstract void ShowName();
+
+        public void ShowName1()
+        {
+            System.Console.WriteLine("Xin chao Product1");
+        }
+    }
+
+    interface IProduct
+    {
+        public void ShowName();
+    }
+    interface IOrder
+    {
+        public void ShowName();
+    }
+    public class Product2 : Product1
+    {
+        public override void ShowName()
+        {
+            System.Console.WriteLine("Xin chao Product2");
+        }
+    }
+
+    public class Product3 : IProduct,IOrder
+    {
+        public void ShowName()
+        {
+
+        }
+    }
     public class Test
     {
         
         
         static void Main()
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            Milk mk = new Milk();
-            Product pt = new Product();
-            pt.ProductInfo();
-            mk.ProductInfo();
+            // Console.OutputEncoding = Encoding.UTF8;
+            // Milk mk = new Milk();
+            // Product pt = new Milk();
+            // System.Console.WriteLine(pt.GetType());
+            // pt.ProductInfo();
+            // mk.ProductInfo();
+            // Product2 pd2 = new Product2();
+            // pd2.ShowName();
+            try
+            {
+                try
+                {
+                    int k = 0;
+                    // double m = 1/k;
+                    throw new Exception("Một ngoại lệ đã xảy ra!");
+                }
+                catch(Exception ex)
+                {
+                    System.Console.WriteLine("Ngoại lệ 1");
+                    throw new Exception("Một ngoại lệ đã xảy ra!");
+                }
+                System.Console.WriteLine("Hihi");
+            }
+            catch(Exception e2)
+            {
+                System.Console.WriteLine(e2.Message);
+            }
+            finally
+            {
+
+            }
+            
             Console.ReadKey();
         }
     }
--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c9ef6a4a8d701b659da8974813d596e8e36745")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
