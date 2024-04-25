3ce68584db051b1c012d3ba8675f5a4892085126
nguyenngocthe1001@gmail.com <nguyenngocthe1001@gmail.com>
2024-04-26 00:40:44 +0700

    26/4/2024 EventHandler and Event

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2eb9980dd56029c0f1ccb600e2a599a313b950d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa714b276881417bde5d55b64bb6300517733903")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
